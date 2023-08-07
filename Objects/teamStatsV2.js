7db409cf45ed67c4077f791d007f37ac36e91f46
Riel <jasper.apos@gmail.com>
2023-08-07 21:15:45 +0800

    Setters to assign

--- Objects/teamStatsV2.js
+++ Objects/teamStatsV2.js
@@ -3,13 +3,23 @@ let team = {
     _games: [],
 
     set players(player) {
-        if (typeof player == 'object') {
+        if (typeof player === 'object') {
             this._players.push(player);
+        }
+    },
+
+    set games(game) {
+        if (typeof game === 'object') {
+            this._games.push(game);
         }
     },
 
     get players() {
         return this._players;
+    },
+
+    get games() {
+        return this._games;
     },
 
     addPlayers(firstName, lastName, age) {
@@ -18,18 +28,34 @@ let team = {
             lastName,
             age,
         }
+    },
+
+    addGames(opponents, teamPoints, opponentPoints) {
+        return {
+            opponents,
+            teamPoints,
+            opponentPoints,
+        }
     }
 }
 
 // const player1 = {
 //     firstName: "Riel", 
-//     lastName: "Jasper", 
+//     lastName: "Apos", 
 //     age: 19,
 // };
 
-const player1 = team.addPlayers("Riel", "Apos", 19);
+const players = [team.addPlayers("Riel", "Apos", 19), team.addPlayers("Aqua", "Hoshino", 16), team.addPlayers("Ruby", "Hoshino", 16)];
+const teamGames = [team.addGames("Wild Cats", 5, 3), team.addGames("Serenity", 1, 5), team.addGames("Knight Raid", 5, 0)];
 
-team.players = player1;
+players.forEach(element => {
+    team.players = element;
+})
+
+teamGames.forEach(element => {
+    team.games = element;
+})
 
 console.log(team.players);
+console.log(team.games);
 
